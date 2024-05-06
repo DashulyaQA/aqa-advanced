d0e722adcea25ffb709fb47fabd9cf672a82f65a
Dariia Lavrenko <dariia.zhabko@gmail.com>
2024-05-06 22:37:26 +0300

    homework 4.1

---
+++ jsBasics/task3.js
@@ -0,0 +1,3 @@
+let workerAge = 17; 
+let isAdult = workerAge >= 18; 
+console.log(isAdult); 
---
+++ jsBasics/task4.js
@@ -0,0 +1,13 @@
+let radius = 15
+let circlRadius = (Math.PI * radius**2).toFixed(2)
+console.log(circlRadius);
+
+let length = 25
+let wigth = 14
+let squareRectangle = (length * wigth).toFixed(2);
+console.log(squareRectangle); 
+
+let radiusCylinder = 23
+let height = 45
+let cylindrVolume = (Math.PI * radiusCylinder**2 * height).toFixed(2);
+console.log(cylindrVolume);
