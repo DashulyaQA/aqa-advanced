7487ab505ab770ca4d4690ec9731c74d22bef981
Dariia Lavrenko <dariia.zhabko@gmail.com>
2024-05-07 01:26:23 +0300

    Homework 5.1

---
+++ conditions&loops/task1.js
@@ -0,0 +1,16 @@
+const avarageGrade = 100 
+
+if (avarageGrade < 60) {
+ console.log('Незвдовільно')
+}  else if (avarageGrade <= 70){
+ console.log ('Задовільно'); 
+} else if (avarageGrade <= 80) {
+    console.log('Добре');
+} else if (avarageGrade <= 90) {
+    console.log('Дуже добре');
+} else if (avarageGrade <= 100) {
+    console.log('Відменно');
+} else {
+    console.log('Невалідна оцінка');
+}
+ 
---
+++ conditions&loops/task3.js
@@ -0,0 +1,12 @@
+const number = 6
+
+for (let i = 1; i <= 10; i++) {
+    console.log(`${number} x ${i} = ${number * i}`)
+}
+
+
+let k = 1; 
+while(k <=10) {
+    console.log(`${number} x ${k} = ${number * k}`)
+    k++
+}
